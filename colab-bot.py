1c8e1a5dc2e51eedebae0645e8280a72ea700dcf
Rishabh Kumar <97455703+rishabhxchoudhary@users.noreply.github.com>
2023-03-08 11:00:34 +0530

    Update colab-bot.py

--- colab-bot.py
+++ colab-bot.py
@@ -1,31 +1,18 @@
-# !pip install pyTelegramBotAPI
-# !pip install praw
-# !pip install requests
-# !pip install beautifulsoup4
-# !pip install redgifs
-
 import telebot
-import threading
+import threading,sys
 import praw
-import random
+from cred import *
 import requests
 from bs4 import BeautifulSoup
 import os
 import redgifs
 import yarl
+import convertapi
+from io import BytesIO
+import tempfile
 
-# Creds
-telegram_token = ""
-username = ""
-password = ""
-client_id = ""
-client_secret=''
-user_agent=""
-mongo_uri=""
-mongo_dbname=""
-mongo_comment_collection=""
-mongo_post_collection=""
-subreddit_name = ""
+API_KEY = "5215172336:AAExMMURMmx-VoOiqj68thVmLJTFrb67gZc"
+convertapi.api_secret = '66kpFfFS194MEvNN'
 
 api = redgifs.API()
 api.login()
@@ -35,6 +22,8 @@ get_gif = api.http.get_gif
 bot = telebot.TeleBot(telegram_token)
 reddit = praw.Reddit(client_id=client_id, client_secret=client_secret,
                      username=username, password=password, user_agent=user_agent)
+
+RUN = True
 
 def check_url_type(url):
     ext = os.path.splitext(url)[1]
@@ -51,12 +40,20 @@ def start(message):
     help_text = "The following commands are available:\n"
     commands = [
         '/start - Start the bot ',
-        '/search - Search subreddits'
+        '/search - Search subreddits',
+        '/meme - Get all meme from a subreddit',
+        '/stop - stop the running commands',
+        '/restart - Re Run the bot script'
     ]
     for cmd in commands:
         help_text += cmd+"\n"
     bot.reply_to(message, help_text)
 
+@bot.message_handler(commands=['restart'])
+def restart(message):
+    print(message.chat.first_name, " :", message.text)
+    bot.reply_to(message, "Restarting...")
+    os.execv(sys.argv[0], sys.argv)
 
 @bot.message_handler(commands=['search'])
 def search(message):
@@ -80,19 +77,68 @@ def search(message):
         o = o+str(i+1)+") "+subs[i]+'\n'
     bot.send_message(chat_id=message.chat.id, text=o)
 
+@bot.message_handler(content_types=['document', 'photo'])
+def convert_to_pdf(message):
+    if (message.caption == "/convert"):
+        print(message.chat.first_name," :",message.caption)
+        try:
+            bot.send_message(message.chat.id, 'Converting...')
+            send_photo_warning = False
+            content_type = message.content_type
+
+            documents = getattr(message, content_type)
+            if not isinstance(documents, list):
+                documents = [documents]
+
+            for doc in documents:
+                file_id = doc.file_id
+                file_info = bot.get_file(file_id)
+                #print(file_info)
+                file_url = 'https://api.telegram.org/file/bot{0}/{1}'.format(API_KEY, file_info.file_path)
+
+                file_data = BytesIO(requests.get(file_url).content)
+                try:
+                    file_name = doc.file_name
+                except:
+                    file_name = file_info.file_path.replace("/", "_")
+                    send_photo_warning = True
+                upload_io = convertapi.UploadIO(file_data, filename=file_name)
+                converted_result = convertapi.convert('pdf', {'File': upload_io})
+                converted_files = converted_result.save_files(tempfile.gettempdir())
+
+                for file in converted_files:
+                    bot.send_document(message.chat.id, open(file, 'rb'))
+
+            if send_photo_warning:
+                bot.send_message(message.chat.id, 'You sent this file as a photo. If you require better quality, please send it as a document.')
+        except:
+            bot.send_message(message.chat.id, 'Connection Timeout Error.')
+
+
+@bot.message_handler(commands=['stop'])
+def stop(message):
+    print(message.chat.first_name, " :", message.text)
+    global RUN
+    RUN=False
+    bot.send_message(chat_id=message.chat.id, text="Stopped all threads.")
 
 @bot.message_handler(commands=['meme'])
 def send_memes(message):
+    global RUN
+    RUN = True
     print(message.chat.first_name, " :", message.text)
     subred = message.text.replace("/meme ", "").strip()
     subred = subred.lower()
     if subred == "/meme":
         subred = "memes"
 
-    def get_memes(message, subred, limit=1000):
+    def get_memes(message, subred, limit=10000):
+        global RUN
         subreddit = reddit.subreddit(subred)
         top = list(subreddit.top(limit=limit))
         for i in top:
+            if not RUN:
+                return
             try:
                 random_sub = i
                 out = ""
@@ -133,5 +179,4 @@ def send_memes(message):
     t.start()
     t.join()
 
-
-bot.polling()
+bot.polling(none_stop=True)
